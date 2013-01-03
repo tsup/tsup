b243a9b0f639149f98936c9915452b40efe16f84
Peter Hajdu <peter.ferenc.hajdu@gmail.com>
2013-01-03 19:14:10 +0100

    implement web interface pushing

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,9 +2,11 @@ class MessagesController < ActionController::Base
 
   def create
     Message.create( params['message'] )
+    redirect_to root_url
   end
 
   def index
+    @message = Message.new
     @messages = Message.all
   end
 
