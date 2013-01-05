94a7fa0d1f323958980a3c5c01734c824c4b9fa1
Peter Hajdu <peter.ferenc.hajdu@gmail.com>
2013-01-05 15:44:10 +0100

    add db migrate script to rename messages to tsups

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,9 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121216181702) do
+ActiveRecord::Schema.define(:version => 20130105181702) do
 
-  create_table "messages", :force => true do |t|
+  create_table "tsups", :force => true do |t|
     t.string   "content"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
